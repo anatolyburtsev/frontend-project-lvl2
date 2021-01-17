df3114028ca0fee15ddf9741f51f718f94aa5769
Anatolii Burtsev <anatolyburtsev@yandex.ru>
2021-01-17 14:59:30 -0800

    add base check github action and codeclimate badge

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/anatolyburtsev/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/anatolyburtsev/frontend-project-lvl2/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/0f59942315935c352ae2/maintainability)](https://codeclimate.com/github/anatolyburtsev/frontend-project-lvl2/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/0f59942315935c352ae2/test_coverage)](https://codeclimate.com/github/anatolyburtsev/frontend-project-lvl2/test_coverage)
 
 ## Usage
 ```sh
--- gendiff.js
+++ gendiff.js
@@ -1,65 +1,65 @@
 #!/usr/bin/env node
 
-import {program} from 'commander';
+import { program } from 'commander';
 import fs from 'fs';
 import path from 'path';
 
-program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .arguments("<filepath1> <filepath2>")
-    .option("-f, --format [type]", "output format")
-    .action((filepath1, filepath2, options) => {
-        const objs = [filepath1, filepath2]
-            .map(normalizePath)
-            .map(fp => fs.readFileSync(fp))
-            .map(JSON.parse);
-        console.log(genDiff(...objs));
-    })
-
 const normalizePath = (filepath) => {
-    if (path.isAbsolute(filepath)) {
-        return path.normalize(filepath);
-    }
-    return path.resolve(process.cwd(), filepath);
-}
-
-const genDiff = (obj1, obj2) => {
-    const keys = [...new Set(Object.keys(obj1).concat(Object.keys(obj2)))];
-    keys.sort();
-
-    const changes = [];
-    keys.map(key => {
-        const value1 = obj1[key];
-        const value2 = obj2[key];
-        if (value1 === value2) {
-            changes.push([" ", key, value1]);
-            return;
-        }
-        if (value1 === undefined) {
-            changes.push(["+", key, value2]);
-            return;
-        }
-        if (value2 === undefined) {
-            changes.push(["-", key, value1]);
-            return;
-        }
-        changes.push(["-", key, value1]);
-        changes.push(["+", key, value2]);
-    })
-
-    return constructAnswer(changes);
-}
+  if (path.isAbsolute(filepath)) {
+    return path.normalize(filepath);
+  }
+  return path.resolve(process.cwd(), filepath);
+};
 
 const constructAnswer = (changes) => {
-    const str = ["{"];
-    changes.map(line => {
-        const [sign, key, value] = line;
-        str.push(`  ${sign} ${key}: ${value}`);
-    })
-    str.push("}");
-    return str.join("\n");
-}
+  const str = ['{'];
+  changes.forEach((line) => {
+    const [sign, key, value] = line;
+    str.push(`  ${sign} ${key}: ${value}`);
+  });
+  str.push('}');
+  return str.join('\n');
+};
+
+const genDiff = (obj1, obj2) => {
+  const keys = [...new Set(Object.keys(obj1).concat(Object.keys(obj2)))];
+  keys.sort();
+
+  const changes = [];
+  keys.forEach((key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    if (value1 === value2) {
+      changes.push([' ', key, value1]);
+      return;
+    }
+    if (value1 === undefined) {
+      changes.push(['+', key, value2]);
+      return;
+    }
+    if (value2 === undefined) {
+      changes.push(['-', key, value1]);
+      return;
+    }
+    changes.push(['-', key, value1]);
+    changes.push(['+', key, value2]);
+  });
+
+  return constructAnswer(changes);
+};
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    const objs = [filepath1, filepath2]
+      .map(normalizePath)
+      .map((fp) => fs.readFileSync(fp))
+      .map(JSON.parse);
+    console.log(genDiff(...objs));
+  });
 
 program.parse();
 
