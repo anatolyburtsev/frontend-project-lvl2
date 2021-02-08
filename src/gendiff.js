96b513270bc0d1a6367b2ce8cc011eb7c4c7303d
Anatolii Burtsev <aburtsev@amazon.com>
2021-02-07 21:38:45 -0800

    some minor refactor after review

--- src/gendiff.js
+++ src/gendiff.js
@@ -12,35 +12,28 @@ import {
   NOT_CHANGED,
 } from './constants.js';
 
-const getListOfKeys = (obj1, obj2) => {
-  const unsortedKeys = [...new Set(Object.keys(obj1).concat(Object.keys(obj2)))]
-    .filter((x) => x);
-  return _.sortBy(unsortedKeys);
-};
+const buildDiffForObjects = (obj1 = {}, obj2 = {}) => {
+  const unsortedKeys = _.union(_.keys(obj1), _.keys(obj2));
+  const keys = _.sortBy(unsortedKeys);
 
-const buildDiffForObjects = (argObj1, argObj2) => {
-  const obj1 = argObj1 ?? {};
-  const obj2 = argObj2 ?? {};
-  const keys = getListOfKeys(obj1, obj2);
-
-  return keys.map((key) => {
+  return keys.flatMap((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
-    if (value1 === value2) {
-      return { key, value: value1, status: NOT_CHANGED };
-    }
-    if (value1 === undefined) {
+    if (!_.has(obj1, key)) {
       return { key, value: value2, status: KEY_ADDED };
     }
-    if (value2 === undefined) {
+    if (!_.has(obj2, key)) {
       return { key, value: value1, status: KEY_REMOVED };
     }
-    if (typeof value1 === 'object' && typeof value2 === 'object') {
-      return { key, value: buildDiffForObjects(value1, value2), status: KEY_UPDATED };
+    if (!_.isEqual(value1, value2)) {
+      if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+        return { key, value: buildDiffForObjects(value1, value2), status: KEY_UPDATED };
+      }
+      return [{ key, value: value1, status: KEY_UPDATED_OLD_VALUE },
+        { key, value: value2, status: KEY_UPDATED_NEW_VALUE }];
     }
-    return [{ key, value: value1, status: KEY_UPDATED_OLD_VALUE },
-      { key, value: value2, status: KEY_UPDATED_NEW_VALUE }];
-  }).flat(1);
+    return { key, value: value1, status: NOT_CHANGED };
+  });
 };
 
 const buildDiff = (filepath1, filepath2, format) => {
